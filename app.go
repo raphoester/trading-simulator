982238d64f505fa0d6d877152d09063225ee7169
raphoester <raphael.oester@ynov.com>
2022-01-03 15:52:42 +0100

    server starting message

--- app.go
+++ app.go
@@ -16,10 +16,10 @@ func (app *App) Run() {
 	app.Router = mux.NewRouter()
 	app.RouteInit()
 	http.ListenAndServe(":8080", app.Router)
+	fmt.Println("Server started on port 8080")
 }
 
 func (app *App) RouteInit() {
 	global := app.Router.PathPrefix("/api/v1").Subrouter()
-
 	global.HandleFunc("/simulate", simulator.SimulatorHandler).Methods("GET")
 }
